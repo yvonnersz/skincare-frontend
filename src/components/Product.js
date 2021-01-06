48c5afe9292b5eba968b1737e9c5c15294513afa
Yvonne <yvonnersz@gmail.com>
2021-01-06 08:03:52 -0800

    add link to product showpage

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,10 +1,12 @@
+import { Link } from 'react-router-dom';
+
 const Product = (props) => {
     return (
         <div class="col-sm-4">
             <div class="card">
                 <img src="..." class="card-img-top" alt="..." />
                 <div class="card-body">
-                    <h5 class="card-title">{props.product.name}</h5>
+                    <h5 class="card-title"><Link to={`/products/${props.product ? props.product.id:null}`}>{props.product.name}</Link></h5>
                     <p class="card-text">{props.product.brand}</p>
                 </div>
             </div>
